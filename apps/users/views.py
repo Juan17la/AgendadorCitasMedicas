f42367ba6b6f22712ab58b090f1443a850617897
Jud1710 <juan.lopezarias@campusucc.edu.co>
2025-04-01 15:16:41 -0500

    main dashboard paciente

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, PasswordResetForm
 
 # Create your views here.
